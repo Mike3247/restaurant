f6abec42e71708480ccc631f74283ccab726714f
Mike Selmani <mike_selmani@hotmail.com>
2023-01-31 09:33:34 -0500

    html, create and comment out html restaurant page text content, js, create h1 header dynamically with js in src folder rendered through to browser with webpack

--- src/index.js
+++ src/index.js
@@ -10,4 +10,19 @@ import _ from 'lodash';
  }
 
  document.body.appendChild(component());
+
+ function component2() {
+  const elementH1 = document.createElement('h1');
+  const textNodeContentForH1 = "Mike's Cozy Restaurant";
+  const textNodeForH1 = document.createTextNode(textNodeContentForH1);
+
+  return elementH1.appendChild(textNodeForH1);
+
+ }
+ 
+ const div1 = document.querySelector("div");
+
+ div1.appendChild(component2());
+
+
  console.log ("Hello, earthling")
