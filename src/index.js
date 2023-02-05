fdd7a1f5c5e465cd886e1b57d24ecf63413d2b52
Mike Selmani <mike_selmani@hotmail.com>
2023-02-04 20:20:56 -0500

    js, rewrite pageload headline and description paragraph DOM creation and appending more adequately

--- src/index.js
+++ src/index.js
@@ -11,31 +11,21 @@
 
 //  document.body.appendChild(component());
 
- 
+const elementDiv1 = document.getElementById("content");
+
 function componentH1() {
-  const elementH1 = document.createElement('h1');
-  const textNodeContentForH1 = "Mike's Cozy Restaurant";
-  const textNodeForH1 = document.createTextNode(textNodeContentForH1);
-
-  return elementH1.appendChild(textNodeForH1);
-
+ const headline = document.createElement("h1");
+ headline.textContent = "Mike's Cozy Restaurant";
+ return headline;
  }
- 
- 
+
+ elementDiv1.appendChild(componentH1());
  console.log(componentH1());
- document.body.appendChild(componentH1());
-
- const elementDiv1 = document.getElementById("content");
  
- function componentDiv1() {
-   
+ function componentDiv1() { 
   const textNodeForDiv1 = document.createTextNode("Mike invites you to enjoy a healthy meal with loved ones at his restaurant. All colors, ages, genders are welcome. Tasty food, healthy food, and a comfortable, safe space to relax.");
-   
-   return elementDiv1.appendChild(textNodeForDiv1);
+  return elementDiv1.appendChild(textNodeForDiv1);
  }
- 
-
  elementDiv1.appendChild(componentDiv1());
 
-
- console.log ("Hello, earthling")
+console.log ("Hello, earthling")
